6f721fcb0e8097a6ffacd8c07da0fe0b69e9d1a7
Virag Brahme <virag.brahme@audetemi.com>
2017-05-25 17:01:22 +0530

    Changed the text titles, removed the unused buttons.

--- CueStacker/Views/ItemsPage.xaml.cs
+++ CueStacker/Views/ItemsPage.xaml.cs
@@ -17,21 +17,14 @@ namespace CueStacker
             BindingContext = viewModel = new ItemsViewModel();
         }
 
-        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
             var item = args.SelectedItem as Item;
             if (item == null)
                 return;
-
-            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
-
+            
             // Manually deselect item
             ItemsListView.SelectedItem = null;
-        }
-
-        async void AddItem_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new NewItemPage());
         }
 
         protected override void OnAppearing()
